cde745256e4048010430e7bc5b7791c3b9f8d677
sankumar14 <58009411+sankumar14@users.noreply.github.com>
2019-11-27 23:26:58 -0600

    Add files via upload

--- guestbook2.py
+++ guestbook2.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 # import boto3
 # from config import S3_BUCKET, S3_KEY, S3_SECRET
